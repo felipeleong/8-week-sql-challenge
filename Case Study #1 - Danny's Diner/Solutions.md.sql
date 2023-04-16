8f7a785bd528ff7e4750f4400198e89034bc19a4
felipeleong <116659667+felipeleong@users.noreply.github.com>
2023-04-15 21:00:44 -0500

    Update and rename Solutions.md.sql to Solutions.md

--- Case Study #1 - Danny's Diner/Solutions.md.sql
+++
@@ -1,8 +0,0 @@
-1.What is the total amount each customer spent at the restaurant?
-SELECT 
-	s.customer_id,
-	sum(m.price) as total_amount
-FROM dannys_diner.sales s
-JOIN dannys_diner.menu m ON m.product_id = s.product_id
-GROUP BY 1
-ORDER BY 1;
